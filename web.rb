57315e9f26dee2380a00cda98b7dba8986db425c
Serena Wales <serenawr@gmail.com>
2012-01-25 16:18:01 -0800

    Template specification

--- web.rb
+++ web.rb
@@ -6,6 +6,8 @@ require './message.rb'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/logger')
 DataMapper.auto_upgrade!
 
+set :haml, :format => :html5
+
 get '/' do
   @messages = Message.all
 
